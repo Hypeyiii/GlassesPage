7a7bcdf054f60593a31535106ec038d070d7dca2
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-03-28 01:42:03 -0600

    re-do the promotion image

--- src/Components/Promotions.tsx
+++ src/Components/Promotions.tsx
@@ -1,61 +1,61 @@
 export const PromotionImage = () => {
   return (
-    <div className="grid grid-cols-12 w-screen items-center justify-center">
-      <div className="col-span-3 relative bg-cover overflow-hidden">
+    <div className="grid grid-cols-4 w-[90%] m-auto h-auto items-center justify-center gap-2 md:gap-8">
+      <div className="col-span-2 md:col-span-1 relative bg-cover overflow-hidden rounded-xl border-2 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-white [&>img]:hover:opacity-100">
         <img
           src="https://www.hawkersco.com/on/demandware.static/-/Library-Sites-Hawkers_Co_SharedLibrary/default/dw0fb86936/images/new-home/slider-collections/610X776_BANNER_COLLECTIONS_1_POLARIZED.jpg"
           alt="Promotion"
-          className="h-[488px] w-full object-cover hover:scale-110 transition-all duration-1000"
+          className="h-[350px] md:h-[488px] w-full object-cover hover:scale-105 transition-all duration-200 opacity-80 dark:opacity-50"
         />
         <div className="absolute bottom-0 p-5">
           <a
             href={"/Sun-Glasses"}
-            className="uppercase text-lg border-2 py-2 px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
+            className="uppercase text-xs md:text-lg border-[1px] py-1 md:py-2 px-2 md:px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
           >
             Polarizadas
           </a>
         </div>
       </div>
-      <div className="col-span-3 relative bg-cover overflow-hidden">
+      <div className="col-span-2 md:col-span-1 relative bg-cover overflow-hidden rounded-xl border-2 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-white [&>img]:hover:opacity-100">
         <img
           src="https://www.hawkersco.com/on/demandware.static/-/Library-Sites-Hawkers_Co_SharedLibrary/default/dw95c7adb9/images/new-home/slider-collections/610X776_BANNER_COLLECTIONS_2_SUMMER_ESSENTIALS.jpg"
           alt="Promotion"
-          className="h-[488px] w-full object-cover hover:scale-110 transition-all duration-1000"
+          className="h-[350px] md:h-[488px] w-full object-cover hover:scale-105 transition-all duration-200 opacity-80 dark:opacity-50"
         />
         <div className="absolute bottom-0 p-5">
           <a
             href={"/Sun-Glasses"}
-            className="uppercase text-lg border-2 py-2 px-4 border-white text-white hover:border-transparent hover:bg-white hover:text-black transition-all duration-300"
+            className="uppercase text-xs md:text-lg border-[1px] py-1 md:py-2 px-2 md:px-4 border-white text-white hover:border-transparent hover:bg-white hover:text-black transition-all duration-300"
           >
             Polarizadas
           </a>
         </div>
       </div>
-      <div className="col-span-3 relative bg-cover overflow-hidden">
+      <div className="col-span-2 md:col-span-1 relative bg-cover overflow-hidden rounded-xl border-2 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-white [&>img]:hover:opacity-100">
         <img
           src="https://www.hawkersco.com/on/demandware.static/-/Library-Sites-Hawkers_Co_SharedLibrary/default/dw49dfd034/images/new-home/slider-collections/610X776_BANNER_COLLECTIONS_5_BEST_SELLERS.jpg"
           alt="Promotion"
-          className="h-[488px] w-full object-cover hover:scale-110 transition-all duration-1000"
+          className="h-[350px] md:h-[488px] w-full object-cover hover:scale-105 transition-all duration-200 opacity-80 dark:opacity-50"
         />
         <div className="absolute bottom-0 p-5">
           <a
             href={"/Sun-Glasses"}
-            className="uppercase text-lg border-2 py-2 px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
+            className="uppercase text-xs md:text-lg border-[1px] py-1 md:py-2 px-2 md:px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
           >
             Polarizadas
           </a>
         </div>
       </div>
-      <div className="col-span-3 relative bg-cover overflow-hidden">
+      <div className="col-span-2 md:col-span-1 relative bg-cover overflow-hidden rounded-xl border-2 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-white [&>img]:hover:opacity-100">
         <img
           src="https://www.hawkersco.com/on/demandware.static/-/Library-Sites-Hawkers_Co_SharedLibrary/default/dw0fb86936/images/new-home/slider-collections/610X776_BANNER_COLLECTIONS_1_POLARIZED.jpg"
           alt="Promotion"
-          className="h-[488px] w-full object-cover hover:scale-110 transition-all duration-1000"
+          className="h-[350px] md:h-[488px] w-full object-cover hover:scale-105 transition-all duration-200 opacity-80 dark:opacity-50"
         />
         <div className="absolute bottom-0 p-5">
           <a
             href={"/Sun-Glasses"}
-            className="uppercase text-lg border-2 py-2 px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
+            className="uppercase text-xs md:text-lg border-[1px] py-1 md:py-2 px-2 md:px-4 border-black hover:border-transparent hover:bg-black hover:text-white transition-all duration-300"
           >
             Polarizadas
           </a>
