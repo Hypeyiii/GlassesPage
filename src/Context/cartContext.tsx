5fa7495ad6583289a6065dc98febc9ca4b7d8772
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-03-28 01:42:12 -0600

    created a context for cart

---
+++ src/Context/cartContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react";
+
+interface Products {
+    id: number;
+    brand: string;
+    price: number;
+    image: string;
+    description: string;
+    quantity: number;
+    total: number;
+    countProducts: number;
+    category: string;
+    genre: string;
+    shape: string;
+}
+
+export interface Cart {
+  children: React.ReactNode;
+}
+export const CartContext = createContext({ 
+    allProducts: [] as Products[],
+    setAllProducts: (value: Products[]) => {value},
+    total: 0,
+    setTotal: (value: number) => {value},
+    countProducts: 0,
+    setCountProducts: (value: number) => {value},
+    isOnCart: false,
+    setIsOnCart: (value: boolean) => {value},  
+});
+
+export function CartProvider({ children }: Cart) {
+  const [allProducts, setAllProducts] = useState<Products[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [countProducts, setCountProducts] = useState<number>(0);
+  const [isOnCart, setIsOnCart] = useState<boolean>(false);
+  return(
+    <CartContext.Provider
+      value={{
+        allProducts,
+        setAllProducts,
+        total,
+        setTotal,
+        countProducts,
+        setCountProducts,
+        isOnCart,
+        setIsOnCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
