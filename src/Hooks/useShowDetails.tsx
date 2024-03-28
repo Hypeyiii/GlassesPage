0e4a30ed310f4b207eeba05fca21aa81367cf43a
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-03-28 01:43:36 -0600

    create custom hook for show details route

---
+++ src/Hooks/useShowDetails.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useCart } from './useCart';
+
+interface Products {
+    id: number;
+    brand: string;
+    price: number;
+    image: string;
+    description: string;
+    quantity: number;
+    total: number;
+    countProducts: number;
+    category: string;
+    genre: string;
+    shape: string;
+}
+
+export function useShowDetails() {
+    const [selectedProduct, setSelectedProduct] = useState<Products | null>(null);
+    const {allProducts} = useCart();
+
+    const showProductDetails = (product: Products): Products[] => {
+        setSelectedProduct(product);
+        return allProducts;
+    };
+
+    return { showProductDetails, selectedProduct };
+}
