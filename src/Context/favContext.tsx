fdb0a11f5c9bb27560de074916ce762ef0677801
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-03-28 01:42:21 -0600

    create a logic for wishlist

---
+++ src/Context/favContext.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, useState } from "react";
+
+interface Products {
+  id: number;
+  brand: string;
+  price: number;
+  image: string;
+  description: string;
+  quantity: number;
+  total: number;
+  countProducts: number;
+  category: string;
+  genre: string;
+  shape: string;
+}
+
+interface Fav {
+  children: React.ReactNode;
+}
+
+export const FavContext = createContext({
+  allFavProducts: [] as Products[],
+  setAllFavProducts: (value: Products[]) => {
+    value;
+  },
+  countFavProducts: 0,
+  setCountFavProducts: (value: number) => {
+    value;
+  },
+  isFav: false,
+  setIsFav: (value: boolean) => {
+    value;
+  },
+});
+
+export function FavProvider({ children }: Fav) {
+  const [allFavProducts, setAllFavProducts] = useState<Products[]>([]);
+  const [countFavProducts, setCountFavProducts] = useState<number>(0);
+  const [isFav, setIsFav] = useState<boolean>(false);
+  return (
+    <FavContext.Provider
+      value={{
+        allFavProducts,
+        setAllFavProducts,
+        countFavProducts,
+        setCountFavProducts,
+        isFav,
+        setIsFav,
+      }}
+    >
+      {children}
+    </FavContext.Provider>
+  );
+}
