65b0a9b81aa112a8baf315153cb29a078de937bc
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-03-28 01:40:30 -0600

    envolving my app with and cart context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { CartProvider } from "./Context/cartContext.tsx";
+import { FavProvider } from "./Context/favContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <CartProvider>
+    <FavProvider>
+      <App />
+    </FavProvider>
+  </CartProvider>
+);
