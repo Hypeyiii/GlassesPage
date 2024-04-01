b24f37b30840af42f5e6405d1529e2c9f98867bf
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-01 17:24:27 -0600

    New file for exporting the products type

---
+++ src/Interface/Products.tsx
@@ -0,0 +1,15 @@
+export interface Products {
+    id: number;
+    brand: string;
+    price: number;
+    image: string;
+    description: string;
+    quantity: number;
+    total: number;
+    countProducts: number;
+    category: string;
+    genre: string;
+    shape: string;
+    color: string;
+    stock: number;
+}
