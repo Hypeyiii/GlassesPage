a729e52652e52c6f3133f3c1926074d9030fc879
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-08 16:48:54 -0600

    New component

---
+++ src/Components/Checkout.tsx
@@ -0,0 +1,8 @@
+const Checkout = () => {
+  return (
+    <div className="mt-24 md:mt-32 text-black dark:text-white w-[70%] m-auto">
+      <h1>Checkout</h1>
+    </div>
+  );
+};
+export default Checkout;
