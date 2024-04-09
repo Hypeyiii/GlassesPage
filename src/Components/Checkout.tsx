5d84413a68ebfd2b704a5964b60874ccf5e1c314
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-09 14:25:09 -0600

    Moved to another folder

--- src/Components/Checkout.tsx
+++
@@ -1,8 +0,0 @@
-const Checkout = () => {
-  return (
-    <div className="mt-24 md:mt-32 text-black dark:text-white w-[70%] m-auto">
-      <h1>Checkout</h1>
-    </div>
-  );
-};
-export default Checkout;
