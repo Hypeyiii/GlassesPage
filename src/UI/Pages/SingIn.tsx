0bf0de7d00f5d693a68a5fbfa03c971a82b3b74b
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-16 02:25:56 -0600

    New component

---
+++ src/UI/Pages/SingIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "../Components/Animations.css";
+import SingInForm from "../Components/SingInForm";
+import LoggedInForm from "../Components/LoggedInForm";
+const SingIn = () => {
+  const [wantToSingIn, setWantToSingIn] = useState(true);
+
+  return (
+    <div
+      className="mt-24 md:mt-32 grid justify-center relative items-center w-[50%] h-[500px] m-auto border-black/20 text-center text-wrap bg-gray-300 dark:bg-white
+    border p-10"
+    >
+      <header className="absolute top-0 left-o right-0 w-full py-10">
+        <nav>
+          <ul className="gap-x-2 grid grid-cols-2 w-[90%] mx-auto mb-5 border-b-[1px] py-2 px-4 border-black text-black text-xl uppercase">
+            <li
+              id="title"
+              className={`${
+                wantToSingIn ? "opacity-100" : "opacity-50"
+              } w-full h-full border-r border-black cursor-pointer`}
+              onClick={() => setWantToSingIn(true)}
+            >
+              <h1>Registrate</h1>
+            </li>
+            <li
+              id="title"
+              className={`${
+                wantToSingIn ? "opacity-50" : "opacity-100"
+              } w-full h-full border-black cursor-pointer`}
+              onClick={() => setWantToSingIn(false)}
+            >
+              <h1>Iniciar Sesión</h1>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{wantToSingIn ? <SingInForm /> : <LoggedInForm />}</main>
+    </div>
+  );
+};
+export default SingIn;
