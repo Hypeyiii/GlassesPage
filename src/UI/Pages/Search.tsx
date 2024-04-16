d8f070153a0c5753a6aea2c5b0011623b723619c
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-16 03:33:21 -0600

    Added count products text

--- src/UI/Pages/Search.tsx
+++ src/UI/Pages/Search.tsx
@@ -12,10 +12,25 @@ const Search = () => {
   let { term } = useParams();
   term = searchTerm;
   return (
-    <div className="w-[90%] md:w-[80%] m-auto text-black dark:text-white">
+    <div className="w-[80%] md:w-[70%] m-auto text-black dark:text-white">
       {searchResults.length > 0 ? (
-        <div className="mt-32 grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-4">
-          <h1 className="text-xl col-span-1 md:col-span-3">Productos para: {term}</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-4">
+          <div className="mt-32 col-span-1 md:col-span-3 text-black dark:text-white font-semibold flex flex-row items-cemter justify-between w-full text-sm md:text-base">
+            <div className="flex flex-row gap-x-1 justify-center items-center">
+              <Link to={"/"} className="font-light hover:underline">
+                Inicio
+              </Link>
+              <p>/</p>
+              <p>
+                Búsqueda: <span className="text-yellow-500">{searchTerm.length > 0 ? searchTerm : "todos"}</span>
+              </p>
+            </div>
+            <div>
+              Mostrando{" "}
+              <span className="text-yellow-500">{searchResults.length}</span>{" "}
+              productos
+            </div>
+          </div>
           {searchResults.map((product) => (
             <ProductItem
               key={product.id}
@@ -35,7 +50,9 @@ const Search = () => {
         <div className="flex flex-col justify-center items-center h-screen gap-2 text-center">
           <h1 className="text-3xl">No hay resultados para: {term}</h1>
           <p>Sigue explorando productos de nuestra tienda!</p>
-          <Link to={"/"} className="px-4 py-2 border">Seguir comprando</Link>
+          <Link to={"/"} className="px-4 py-2 border">
+            Seguir comprando
+          </Link>
         </div>
       )}
     </div>
