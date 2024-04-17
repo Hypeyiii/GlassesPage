bea0feb00c9e4333c7fc44acefdce6204309c0b4
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-17 10:31:19 -0600

    Fixed design on mobile size

--- src/UI/Pages/SingIn.tsx
+++ src/UI/Pages/SingIn.tsx
@@ -7,10 +7,10 @@ const SingIn = () => {
 
   return (
     <div
-      className="mt-24 md:mt-32 grid justify-center relative items-center w-[50%] h-[500px] m-auto border-black/20 text-center text-wrap bg-gray-300 dark:bg-white
+      className="mt-24 md:mt-32 grid justify-center relative items-center w-[80%] md:w-[50%] h-[500px] m-auto border-black/20 text-center text-wrap bg-gray-300 dark:bg-white
     border p-10"
     >
-      <header className="absolute top-0 left-o right-0 w-full py-10">
+      <header className="absolute top-0 left-o right-0 w-full py-5 md:py-10">
         <nav>
           <ul className="gap-x-2 grid grid-cols-2 w-[90%] mx-auto mb-5 border-b-[1px] py-2 px-4 border-black text-black text-xl uppercase">
             <li
@@ -20,7 +20,7 @@ const SingIn = () => {
               } w-full h-full border-r border-black cursor-pointer`}
               onClick={() => setWantToSingIn(true)}
             >
-              <h1>Registrate</h1>
+              <h1 className="text-sm md:text-base lg:text-lg">Registrate</h1>
             </li>
             <li
               id="title"
@@ -29,7 +29,7 @@ const SingIn = () => {
               } w-full h-full border-black cursor-pointer`}
               onClick={() => setWantToSingIn(false)}
             >
-              <h1>Iniciar Sesión</h1>
+              <h1 className="text-sm md:text-base lg:text-lg">Iniciar Sesión</h1>
             </li>
           </ul>
         </nav>
