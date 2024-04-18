c9f1d4953e43a36d6dc0f569977a7e88f048bca5
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-17 22:18:01 -0600

    Added carrousel promotion

--- src/UI/Pages/HomePage.tsx
+++ src/UI/Pages/HomePage.tsx
@@ -1,16 +1,20 @@
-import { MainPromotion, PromotionImage, PromotionLogin } from "../Components/Promotions";
+import {
+  Carrousel,
+  PromotionImage,
+  PromotionLogin,
+} from "../Components/Promotions";
 import Section from "../Components/HomePage-Sections";
 
 export default function HomePage() {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-32 gap-y-20 w-[80%] md:w-[70%] m-auto">
-        <MainPromotion/>
-        <Section category="man" collection="hombres"/>
+        <Carrousel />
+        <Section category="man" collection="hombres" />
         <PromotionLogin />
-        <Section category="woman" collection="mujer"/>
+        <Section category="woman" collection="mujer" />
         <PromotionImage />
-        <Section category="Sun" collection="sol"/>
+        <Section category="Sun" collection="sol" />
         <Section category="Vision" collection="vision" />
       </div>
     </>
