847f3e43180fdbbd75f3f04e81d0f08e7b1aabc4
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-15 15:54:58 -0600

    🆕 Add mobile desing on dashboard

--- src/UI/Pages/DashboardUsers.tsx
+++ src/UI/Pages/DashboardUsers.tsx
@@ -45,22 +45,22 @@ const DashboardProducts = () => {
         <table className="w-full text-center mt-5 border-[0.5px] border-collapse border-white">
           <thead className="bg-black dark:bg-white text-white dark:text-black font-bold border-[0.5px] border-collapse border-black">
             <tr className="border-[0.5px] border-collapse border-white">
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base hidden md:block">
                 Id
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base">
                 Username
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base">
                 Email
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base">
                 Rol
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base">
                 Eliminar
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-xs md:text-base">
                 Editar
               </th>
             </tr>
@@ -69,30 +69,30 @@ const DashboardProducts = () => {
             {users &&
               users.map((userData) => (
                 <tr key={userData.id}>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base hidden md:table-cell">
                     {userData.id}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base">
                     {userData.username}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base">
                     {userData.email}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base">
                     {userData.role}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base">
                     <button
-                      className="bg-red-500 text-white p-1 text-xs"
+                      className="bg-red-500 text-white text-xs md:text-base p-1"
                       onClick={() => handleDelete(userData.id)}
                     >
                       Eliminar
                     </button>
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-xs md:text-base">
                     <Link
                       to={`Edit/${userData.id}`}
-                      className="bg-blue-500 text-white p-1 text-xs"
+                      className="bg-blue-500 text-white p-1 text-xs md:text-base"
                     >
                       Editar
                     </Link>
--- src/UI/Pages/DashbordProducts.tsx
+++ src/UI/Pages/DashbordProducts.tsx
@@ -4,6 +4,8 @@ import "../Components/Animations.css";
 import { Link } from "react-router-dom";
 import { Products } from "../Interface/Products";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+import { BiEdit } from "react-icons/bi";
 
 const DashboardProducts = () => {
   const [products, setProducts] = useState<Products[]>([]);
@@ -35,7 +37,7 @@ const DashboardProducts = () => {
   }, []);
 
   return (
-    <div className="mt-32 text-black dark:text-white w-[85%] mx-auto">
+    <div className="mt-32 text-black dark:text-white md:w-[90%] w-[85%] mx-auto px-3">
       <h1 className="text-center text-2xl">Dashboard Products</h1>
       {loading ? (
         <div className="flex flex-col gap-2 justify-center items-center h-full mt-5">
@@ -43,31 +45,31 @@ const DashboardProducts = () => {
           <AiOutlineLoading3Quarters className="spin size-32 md:size-52" />
         </div>
       ) : (
-        <table className="w-full text-center mt-5 border-[0.5px] border-collapse border-white">
+        <table className="w-full text-center mt-5 border-[0.5px] border-collapse border-white m-auto">
           <thead className="bg-black dark:bg-white text-white dark:text-black font-bold border-[0.5px] border-collapse border-black">
             <tr className="border-[0.5px] border-collapse border-white">
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black hidden md:block">
                 Id
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Marca
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Descripción
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Precio
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Stock
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Imagen
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Eliminar
               </th>
-              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black">
+              <th className="py-3 border-[0.5px] border-collapse border-white dark:border-black text-[10px] md:text-base">
                 Editar
               </th>
             </tr>
@@ -76,42 +78,42 @@ const DashboardProducts = () => {
             {products &&
               products.map((product) => (
                 <tr key={product.id}>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white hidden md:table-cell">
                     {product.id}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     {product.brand}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     {product.description}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     {product.price}
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     {product.stock}
                   </td>
-                  <td className="size-28 py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="size-16 md:size-28 py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     <img
                       src={product.image}
                       alt={product.brand}
                       className="w-[80%] h-auto m-auto"
                     />
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-1 md:py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     <button
-                      className="bg-red-500 text-white p-1 text-xs"
+                      className="bg-red-500 rounded-full p-1 text-white md:p-1 text-xs"
                       onClick={() => handleDelete(product.id)}
                     >
-                      Eliminar
+                      <RxCross1 className="size-3 md:size-6" />
                     </button>
                   </td>
-                  <td className="py-3 border-[0.5px] border-collapse border-black dark:border-white">
+                  <td className="py-1 md:py-3 border-[0.5px] border-collapse border-black dark:border-white text-[10px] ">
                     <Link
                       to={`Edit/${product.id}`}
-                      className="bg-blue-500 text-white p-1 text-xs"
+                      className="text-blue-500 md:p-1 text-xs"
                     >
-                      Editar
+                      <BiEdit className="size-4 md:size-6 m-auto" />
                     </Link>
                   </td>
                 </tr>
