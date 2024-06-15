1a6bbaf0ae1bbe4e9647ef3d72021412878b8332
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-15 14:22:14 -0600

    ⬆️ Add new routes and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -16,23 +16,70 @@ import SingIn from "./UI/Pages/SingIn";
 import MyAccount from "./UI/Pages/MyAccount";
 import Search from "./UI/Pages/Search";
 import Collection from "./UI/Pages/Collection";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useContext } from "react";
+import { AuthContext } from "./UI/Context/authContext";
+import { STRIPE_SECRET_KEY } from "./config.js";
+import Dashboard from "./UI/Pages/Dashboard";
+import DashboardProducts from "./UI/Pages/DashbordProducts.js";
+import ProductEdit from "./UI/Pages/ProductEdit.js";
+import ProductCreate from "./UI/Pages/ProductCreate";
+import DashboardUsers from "./UI/Pages/DashboardUsers";
+import useUsers from "./UI/Hooks/useUsers.js";
 
 export default function App() {
+  const { isLogged } = useContext(AuthContext);
+  const { userData } = useUsers();
+
+  const stripePromise = loadStripe(STRIPE_SECRET_KEY);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="Cart" element={<Cart />} />
-          <Route path="Wishlist" element={<Wishlist />} />
+          <Route path="Cart" element={isLogged ? <Cart /> : <SingIn />} />
+          <Route
+            path="Wishlist"
+            element={isLogged ? <Wishlist /> : <SingIn />}
+          />
           <Route path="collection/:section" element={<Collection />} />
           <Route path=":category/Product/:id" element={<Details />} />
           <Route path="*" element={<ErrorPage />} />
-          <Route path="User" element={<Loggin />} />
+          {isLogged && <Route path="User" element={<Loggin />} />}
           <Route path="Sing-In" element={<SingIn />} />
           <Route path="My-account" element={<MyAccount />} />
-          <Route path="Cart/Checkout" element={<Checkout />} />
+          <Route
+            path="Cart/Checkout"
+            element={
+              <Elements stripe={stripePromise}>
+                <Checkout />
+              </Elements>
+            }
+          />
           <Route path="/search/:term" element={<Search />} />
+          {userData?.role === "Admin" && (
+            <>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route
+                path="/Dashboard/Products"
+                element={<DashboardProducts />}
+              />
+               <Route
+                path="/Dashboard/Products/Edit/:id"
+                element={<ProductEdit />}
+              />
+               <Route
+                path="/Dashboard/Products/Create"
+                element={<ProductCreate />}
+              />
+               <Route
+                path="/Dashboard/Users"
+                element={<DashboardUsers />}
+              />
+            </>
+          )}
         </Route>
       </>
     )
