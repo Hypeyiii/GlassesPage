9ab444f5ed756855006d38e1787f238664e8a1a9
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-15 15:55:15 -0600

    🔄️ Change color design

--- src/UI/Pages/SingIn.tsx
+++ src/UI/Pages/SingIn.tsx
@@ -7,17 +7,16 @@ const SingIn = () => {
 
   return (
     <div
-      className="mt-24 md:mt-32 grid justify-center relative items-center w-[80%] md:w-[50%] h-[500px] m-auto border-black/20 text-center text-wrap bg-gray-300 dark:bg-white
-    border p-10"
+      className="mt-24 md:mt-32 grid justify-center relative items-center w-[80%] md:w-[50%] h-[600px] m-auto text-center text-wrap text-black dark:text-white p-10"
     >
       <header className="absolute top-0 left-o right-0 w-full py-5 md:py-10">
         <nav>
-          <ul className="gap-x-2 grid grid-cols-2 w-[90%] mx-auto mb-5 border-b-[1px] py-2 px-4 border-black text-black text-xl uppercase">
+          <ul className="gap-x-2 grid grid-cols-2 w-[90%] mx-auto mb-5 border-b-[1px] py-2 px-4 border-black dark:border-white text-black dark:text-white text-xl uppercase">
             <li
               id="title"
               className={`${
                 wantToSingIn ? "opacity-100" : "opacity-50"
-              } w-full h-full border-r border-black cursor-pointer`}
+              } w-full h-full border-r border-black dark:border-white cursor-pointer`}
               onClick={() => setWantToSingIn(true)}
             >
               <h1 className="text-sm md:text-base lg:text-lg">Registrate</h1>
