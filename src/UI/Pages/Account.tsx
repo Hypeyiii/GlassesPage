40e92bd30e8801c4d202f2a3dc2afef09e73ef78
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-17 13:06:32 -0600

    ✅ Add loading funcionality and fixed fetch peitition

--- src/UI/Pages/Account.tsx
+++ src/UI/Pages/Account.tsx
@@ -1,7 +1,8 @@
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import useAuth from "../Hooks/useAuth";
 import { useEffect, useState } from "react";
 import { Users } from "../Interface/Users";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import "../Components/Animations.css";
 
 const Account = () => {
   const { logout, user } = useAuth();
@@ -10,46 +11,59 @@ const Account = () => {
   const [userData, setUserData] = useState<Users | null>(null);
 
   useEffect(() => {
-    try {
-      fetch(`https://glasses-page-api-rest-production.up.railway.app/users/${user?.id}`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => setUserData(data));
-    } catch (error) {
-      console.error("Error al obtener usuario", error);
-    }
-  });
+    const fetchUserData = async () => {
+      if (user?.id) {
+        setLoading(true);
+        try {
+          const response = await fetch(
+            `https://glasses-page-api-rest-production.up.railway.app/users/${user.id}`,
+            {
+              method: "GET",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const data = await response.json();
+          setUserData(data);
+        } catch (error) {
+          console.error("Error al obtener usuario", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [user?.id]);
 
   return (
     <div className="m-auto w-[85%] mt-32 text-black dark:text-white">
       <h1 className="text-2xl font-bold text-center">Mi cuenta</h1>
       <p className="text-center text-gray-500">
-        Bienvenido {userData?.username}
+        Bienvenido {loading ? "Obteniendo..." : userData?.username}
       </p>
       <hr className="my-4" />
-      <h2 className="text-lg font-bold">Datos de usuario</h2>
-      <p>ID de usuario: {userData?.id}</p>
-      <p>Nombre de usuario: {userData?.username}</p>
-      <p>Correo electrónico: {userData?.email}</p>
-      <p>Rol: {userData?.role}</p>
+      {loading ? (
+        <div className="flex m-auto mt-2 justify-center items-center size-32 text-black dark:text-white">
+          <AiOutlineLoading3Quarters className="spin size-32 text-black dark:text-white" />
+        </div>
+      ) : (
+        <>
+          <h2 className="text-lg font-bold">Datos de usuario</h2>
+          <p>ID de usuario: {userData?.id}</p>
+          <p>Nombre de usuario: {userData?.username}</p>
+          <p>Correo electrónico: {userData?.email}</p>
+          <p>Rol: {userData?.role}</p>
+        </>
+      )}
       <hr className="my-4" />
       <button
         className="px-4 py-2 border rounded-xl m-auto flex flex-row gap-1 items-center"
-        onClick={() => logout(setLoading)}
+        onClick={() => logout()}
       >
-        {loading ? (
-          <>
-            <AiOutlineLoading3Quarters className="spin" />
-            Cerrando Sesión
-          </>
-        ) : (
-          "Cerrar sesión"
-        )}
+        Cerrar Sesión
       </button>
     </div>
   );
