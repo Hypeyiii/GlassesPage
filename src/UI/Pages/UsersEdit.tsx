993358695491af795b3e4792bbd1f9fb4d7812fc
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-24 02:53:09 -0600

    🛠️ Fix const name

--- src/UI/Pages/UsersEdit.tsx
+++ src/UI/Pages/UsersEdit.tsx
@@ -10,37 +10,44 @@ const UsersEdit = () => {
   const [success, setSuccess] = useState("");
 
   useEffect(() => {
-    const getProduct = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(`https://glasses-page-api-rest-production.up.railway.app/users/${id}`, {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-        setUser(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error al obtener producto", error);
-        setLoading(false);
+    const fetchUserData = async () => {
+      if (id) {
+        try {
+          const response = await fetch(
+            `https://glasses-page-api-rest-production.up.railway.app/users/${id}`,
+            {
+              method: "GET",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const data = await response.json();
+          setUser(data);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error al obtener usuario", error);
+        }
       }
     };
-    getProduct();
+
+    fetchUserData();
   }, [id]);
 
   const handleEdit = async () => {
     try {
       setLoading(true);
-      await fetch(`https://glasses-page-api-rest-production.up.railway.app/users/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
+      await fetch(
+        `https://glasses-page-api-rest-production.up.railway.app/users/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
       setSuccess("Producto editado correctamente!");
       setLoading(false);
     } catch (error) {
@@ -66,14 +73,12 @@ const UsersEdit = () => {
       ) : (
         <>
           <span className="flex flex-col md:flex-row gap-2 justify-start md:justify-between w-full md:w-1/3 items-start md:items-center">
-            <label>Brand</label>
+            <label>Email</label>
             <input
               className="bg-black text-white dark:bg-white dark:text-black rounded-xl p-3 w-full md:w-[80%] text-sm"
               type="text"
               value={user.email}
-              onChange={(e) =>
-                setUser({ ...user, email: e.target.value })
-              }
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
           </span>
           <span className="flex flex-col md:flex-row gap-2 justify-start md:justify-between w-full md:w-1/3 items-start md:items-center">
@@ -82,20 +87,7 @@ const UsersEdit = () => {
               className="bg-black text-white dark:bg-white dark:text-black rounded-xl p-3 w-full md:w-[80%] text-sm"
               type="text"
               value={user.username}
-              onChange={(e) =>
-                setUser({ ...user, username: e.target.value })
-              }
-            />
-          </span>
-          <span className="flex flex-col md:flex-row gap-2 justify-start md:justify-between w-full md:w-1/3 items-start md:items-center">
-            <label>Password</label>
-            <input
-              className="bg-black text-white dark:bg-white dark:text-black rounded-xl p-3 w-full md:w-[80%] text-sm"
-              type="password"
-              value={user.password}
-              onChange={(e) =>
-                setUser({ ...user, password: e.target.value })
-              }
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
           </span>
           <span className="flex flex-col md:flex-row gap-2 justify-start md:justify-between w-full md:w-1/3 items-start md:items-center">
@@ -104,9 +96,7 @@ const UsersEdit = () => {
               className="bg-black text-white dark:bg-white dark:text-black rounded-xl p-3 w-full md:w-[80%] text-sm"
               type="text"
               value={user.role}
-              onChange={(e) =>
-                setUser({ ...user, role: e.target.value })
-              }
+              onChange={(e) => setUser({ ...user, role: e.target.value })}
             />
           </span>
         </>
